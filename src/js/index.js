5c05c5027217bd95ed4e8e5b81bbcf28fc879ca7
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-06-13 01:28:52 -0500

    pendiente axios

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,62 @@ const form = document.getElementById('search-form');
 const gallery = document.getElementById('gallery'); // espacio para las tarjetas de imagenes
 const loadMoreBtn = document.getElementById('load-more'); //botom para cargar mas imagenes
 
+let lightbox;
 let page = 1;
 let perPage = 40;
 loadMoreBtn.style.display = 'none';
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+window.addEventListener('scroll', scrollImg);
+
+// funcions asincronica par buscar imagenes
+async function searchImages(searchQuery) {
+	const apiKey = '37119471-67d3015a83a24c7694e1d7310'; // Replace with your Pixabay API key
+	const apiUrl = `https://pixabay.com/api/?
+	key=${apiKey}
+	&q=${searchQuery}
+	&image_type=photo
+	&orientation=horizontal
+	&safesearch=true
+	&page=${page}
+	&per_page=${perPage}`;
+
+	try {
+		const response = await fetch(apiUrl);
+		const data = await response.json();
+
+		if (data.totalHits === 0) {
+			Notiflix.Notify.failure(
+				'Sorry, there are no images matching your search query. Please try again.'
+			);
+			return;
+		}
+
+		data.hits.forEach(image => {
+			const card = createPhotoCard(image);
+			gallery.appendChild(card);
+		});
+		Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+		if (data.totalHits <= page * 40) {
+			Notiflix.Notify.warning(
+				"We're sorry, but you've reached the end of search results."
+			);
+		}
+
+		if (lightbox) {
+			lightbox.refresh();
+		} else {
+			lightbox = new SimpleLightbox('.photo-card a');
+		}
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
+
+// funcion para buscar imagenes
+function onSearch(e) {
 	e.preventDefault();
 	const searchQuery = form.searchQuery.value.trim();
 	if (searchQuery === '') return;
@@ -20,16 +71,30 @@ form.addEventListener('submit', e => {
 	//page = 1;
 	searchImages(searchQuery);
 	loadMoreBtn.style.display = 'none';
-});
+}
 
-//evento del botom load.more
-loadMoreBtn.addEventListener('click', () => {
+// funcion para cargar mas imagenes
+function onLoadMoreBtn() {
 	const searchQuery = form.searchQuery.value.trim();
-	if (searchQuery === '') return;
+	if (searchQuery === '') {
+		return;
+	}
 
 	page += 1;
 	searchImages(searchQuery);
-});
+}
+
+// funcion para cargar mas imagenes con scroll
+function scrollImg() {
+	// Verificar si el usuario ha llegado al final de la página
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+		// Cargar más imágenes aquí
+		const searchQuery = form.searchQuery.value.trim();
+		if (searchQuery === '') return;
+		page += 1;
+		searchImages(searchQuery);
+	}
+}
 
 // funcion para insertar elementos al dom
 function createPhotoCard(image) {
@@ -53,59 +118,3 @@ function createPhotoCard(image) {
 
 	return tempContainer.firstElementChild;
 }
-// funcion para noticar erro
-function showNotification(message) {
-	Notiflix.Notify.failure(message, {
-		position: 'right-up',
-		timeout: 2000,
-		borderRadius: '3px',
-	});
-}
-
-// funcions asincronica par buscar imagenes
-async function searchImages(searchQuery) {
-	const apiKey = '37119471-67d3015a83a24c7694e1d7310'; // Replace with your Pixabay API key
-	const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
-	try {
-		const response = await fetch(apiUrl);
-		const data = await response.json();
-
-		if (data.totalHits === 0) {
-			showNotification(
-				'Sorry, there are no images matching your search query. Please try again.'
-			);
-			return;
-		}
-
-		data.hits.forEach(image => {
-			const card = createPhotoCard(image);
-			gallery.appendChild(card);
-		});
-
-		if (data.totalHits <= page * 40) {
-			showNotification(
-				"We're sorry, but you've reached the end of search results."
-			);
-		}
-
-		lightbox = new SimpleLightbox('.photo-card a');
-		//showNotification(`Hooray! We found ${data.totalHits} images.`);
-	} catch (error) {
-		console.error('Error:', error);
-	}
-}
-
-// evento del cliente en el botom
-
-window.addEventListener('scroll', function () {
-	// Verificar si el usuario ha llegado al final de la página
-	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-		// Cargar más imágenes aquí
-		const searchQuery = form.searchQuery.value.trim();
-		if (searchQuery === '') return;
-
-		page += 1;
-		searchImages(searchQuery);
-	}
-});
